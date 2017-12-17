7ab8b2162070c5b1cdef158086b4d47712909dd6
Abel Agoi Adeyemi <agoiabeladeyemi@gmail.com>
2017-12-17 23:10:16 +0100

    first component and template for reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'reactive forms';
+  myGroup: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.myGroup = new FormGroup({
+      'firstname': new FormControl(''),
+      'password': new FormControl('')
+    });
+  }
+
 }
